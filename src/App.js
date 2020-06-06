7cd81cd4c489610d452a6afe32fe4c5001a15d4b
amirmaavia <Maaviyamansoori222@gmail.com>
2020-06-06 12:58:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1 style={{fontFamily: "bold"}} >COVID-19</h1>
+    <h1 style={{fontFamily: "bold"}} >Covid-19</h1>
     <Livestat />
     <Contries />
     </div>
